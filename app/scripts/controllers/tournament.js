8bdd0156b1d899e442f8db1f12519cf16901fd33
MagRelo <Magrelo404@gmail.com>
2014-03-26 19:14:25 -0600

    refine views

--- app/scripts/controllers/tournament.js
+++ app/scripts/controllers/tournament.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('fantasyGolfApp')
-  .controller('TournamentCtrl', function ($scope, $http) {
+  .controller('TournamentCtrl', function ($scope, Team) {
+
+    if($scope.currentUser.teamId){
+      $scope.team = Team.get({id: $scope.currentUser.teamId})
+    }
 
   });
--- app/scripts/services/leagues.js
+++ app/scripts/services/leagues.js
@@ -1,17 +1,13 @@
 'use strict';
 
 angular.module('fantasyGolfApp')
-  .service('Leagues', function Leagues() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-
-    this.leagues = [
-      {name: 'league 1'},
-      {name: 'league 2'},
-      {name: 'league 3'},
-      {name: 'league 4'},
-      {name: 'league 5'},
-      {name: 'league 6'},
-      {name: 'league 7'}
-    ];
-
+  .factory('League', function ($resource) {
+    return $resource('/api/league/:id', {
+      id: '@_id'
+    }, { //parameters default
+      update: {
+        method: 'PUT',
+        params: {}
+      }
+    });
   });
