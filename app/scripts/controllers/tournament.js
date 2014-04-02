a40c2a17ba40c1ce8828aae27c6c191a88690cee
MagRelo <Magrelo404@gmail.com>
2014-04-02 12:14:48 -0600

    styling improvments, remove columns for xs screens

--- app/scripts/controllers/tournament.js
+++ app/scripts/controllers/tournament.js
@@ -3,20 +3,40 @@
 angular.module('fantasyGolfApp')
   .controller('TournamentCtrl', function ($scope, teamModel, pga, Team) {
 
-    //1. Display Tournament info
-    pga.setup({},
-      function(data){
-        $scope.event = data.event;
-        $scope.players = data.field;
-        $scope.courses = data.courseInfos;
-      },
-      function(error){ $scope.error = error; }
-    );
 
-    //2. Display Team Info
+    //2. Get Team Info
     Team.get({id: $scope.currentUser.teamId},
       function(data){
         $scope.team = data;
+
+
+        //1. Get Tournament info
+        pga.setup({},
+          function(data){
+            $scope.event = data.event;
+            $scope.courses = data.courseInfos;
+            $scope.players = data.field;
+
+
+            //loop through team players
+            angular.forEach($scope.team.players, function(teamPlayer, teamIndex){
+
+              //loop through field players
+              angular.forEach(data.field, function(fieldPlayer){
+
+                if(teamPlayer.pgaId == fieldPlayer.id){
+                  angular.extend($scope.team.players[teamIndex], fieldPlayer);
+                }
+
+              });
+
+            });
+
+          },
+          function(error){ $scope.error = error; }
+        );
+
+
       },
       function(error){ $scope.error = error; }
     );
