2498f7c7665d0ec87e0f1efe77e61f32ab6814ee
MagRelo <Magrelo404@gmail.com>
2014-04-10 19:34:24 -0600

    update setupTournament file to support heroku and local dev

--- bin/setupTournament.js
+++ bin/setupTournament.js
@@ -1,46 +1,27 @@
 'use strict';
-//var mongoose = require('mongoose'),
-//  Schema = mongoose.Schema,
-//  request = require('request');
-
 var request = require('request');
-var databaseUrl = "ballstrikers"; // "username:password@example.com/mydb"
+var databaseUrl = process.env.MONGOHQ_URL || "ballstrikers"; // heroku MongoHQ || local default
 var collections = ["tournaments"];
 var db = require("mongojs").connect(databaseUrl, collections);
-
-
-/**
- * Tournament Schema
- */
-//var TournamentSchema = new Schema({
-//  modified: { type: Date, default: Date.now },
-//  event: {},
-//  courseInfos: [],
-//  field: []
-//});
-//
-///**
-// * Register Model
-// */
-//mongoose.model('Tournament', TournamentSchema);
-//var Tournament = mongoose.model('Tournament');
 
 /**
  * Get and Save tournament.js
  */
-
-
 var tournamentSetup = function() {
 
   request({uri: 'http://www.pgatour.com/data/r/current/setup.json', json: true},
     function(error, response, body) {
-      if(error){ console.log('error1') }
+      if(error){
+        console.log(error);
+        return;
+      }
 
       console.log('setup record retrieved');
 
       //check for tournament object
       if(typeof body.trn === "undefined"){
         console.log('tournament setup record retrieved but undefined');
+        return;
       }
 
       var setupObj = {
@@ -52,19 +33,14 @@ var tournamentSetup = function() {
 
       db.tournaments.save(setupObj,
         function(error, saved){
-          if(error || !saved){console.log('setup file not saved: ' + error);}
+          if(error || !saved){
+            console.log('setup file not saved: ' + error);
+            return;
+          }
 
-          console.log('setup record saved');
+          console.log('setup record saved: ' + saved);
         });
 
-
-      //save
-//      var setupFile = new Tournament(body.trn);
-//      setupFile.save(function(err){
-//        if(err){console.log('error2');}
-//
-//        console.log('setup record saved');
-//      })
     }
   )
 };
