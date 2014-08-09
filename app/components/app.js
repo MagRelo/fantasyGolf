6daf4ef64b7900f70b7ca361190d469eb4d5b52b
MagRelo <Magrelo404@gmail.com>
2014-08-09 11:17:56 -0600

    add firebase

--- app/components/app.js
+++ app/components/app.js
@@ -5,7 +5,8 @@ angular.module('fantasyGolfApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'firebase'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
