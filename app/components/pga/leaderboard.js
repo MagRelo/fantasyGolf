ba10531b143319c2f7b05921e2df10009f6e8f9a
MagRelo <Magrelo404@gmail.com>
2014-08-21 13:06:26 -0600

    add loading spinner to leaderboard

--- app/components/pga/leaderboard.js
+++ app/components/pga/leaderboard.js
@@ -3,15 +3,21 @@
 angular.module('fantasyGolfApp')
   .controller('LeaderboardCtrl', function ($scope, $routeParams, pga) {
 
+    $scope.dataLoaded = false;
+
     pga.getLeaderboard()
       .then(
         function(result){
           $scope.players = result.data.players;
           $scope.courses = result.data.courses;
           $scope.info = result.data.info;
+
+          $scope.dataLoaded = true;
         },
         function(error){
           $scope.error = error;
+
+          $scope.dataLoaded = true;
         }
     );
 
