3c8f51db6175dba8beabd2724db9483541cc43bd
MagRelo <Magrelo404@gmail.com>
2014-08-29 12:24:52 -0600

    parse scores into floats for sorting

--- app/components/league/listLeagues.js
+++ app/components/league/listLeagues.js
@@ -6,9 +6,25 @@ angular.module('fantasyGolfApp')
 
     var teamId = $scope.currentUser.teamId;
 
+
+    //parse scores into floats for sorting
+    var parseScores = function(leagues){
+      angular.forEach(leagues, function(league){
+        angular.forEach(league.leaderboard, function(team,index){
+          league.leaderboard[index].score = parseFloat(team.score);
+        });
+      });
+      return leagues;
+    };
+
+
+
     Team.getTeam(teamId)
       .then(function(result){
         $scope.team = result.data;
+
+        $scope.team.leagues = parseScores($scope.team.leagues);
+
       },function(error){
         $scope.error = error;
       }
